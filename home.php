acc21e83f35b124d444c10a5cbeca0d496a2b870
Sylvester976 <slyoti2385@gmail.com>
2021-09-02 17:51:37 +0300

    likizo

---
+++ home.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>LIKIZO</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="assets/home/css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" href="assets/home/css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="assets/home/css/responsive.css">
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="assets/home/css/jquery.mCustomScrollbar.min.css">
+
+   </head>
+   <!-- body -->
+   <body class="main-layout">
+      <!-- loader  -->
+      <div class="loader_bg">
+         <div class="loader"><img src="images/loading.gif" alt="#" /></div>
+      </div>
+      <!-- end loader -->
+      <!-- header -->
+      <header>
+         <!-- header inner -->
+         <div class="header">
+         <div class="container">
+            <div class="row">
+               <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                  <div class="full">
+                     <div class="center-desk">
+                        <div class="logo"> <a href="home.php">Likizo</a> </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                  <div class="menu-area">
+                     <div class="limit-box">
+                        <nav class="main-menu">
+                           <ul class="menu-area-main">
+                              <li class="active"> <a href="home.php">Home</a> </li>
+                              <li> <a href="#about">About</a> </li>
+                              <li> <a href="login.php">Get started</a> </li>
+                              <li class="mean-last"> <a href="#"><img src="images/search_icon.png" alt="#" /></a> </li>
+                           </ul>
+                        </nav>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- end header inner -->
+      </header>
+      <!-- end header -->
+      <section class="slider_section">
+         <div id="myCarousel" class="carousel slide banner-main" data-ride="carousel">
+            <div class="carousel-inner">
+               <div class="carousel-item active">
+                  <img class="first-slide" src="images/banner2.jpg" alt="First slide">
+                  <div class="container">
+                     <div class="carousel-caption relative">
+                        <h1>LIKIZO LEAVE MANAGEMENT SOLUTIONS</h1>
+                        <p>This is the official website of Likizo. Likizo is an organization that offers employees’ absence and leave accruals services to firms, companies and organization.</p>
+                        <a  href="#about">Read More</a>
+                     </div>
+                  </div>
+               </div>
+               <div class="carousel-item">
+                  <img class="second-slide" src="images/banner3.jpg" alt="Second slide">
+                  <div class="container">
+                     <div class="carousel-caption relative">
+                        <h1>Creativity and technology</h1>
+                        <p>Likizo has been established recently with the aim of helping various human resource departments in assigning leave to staff member, manage employee’s absence, email notification, automated leave approval and report generation in a firm or organization from any region or locality at ease of a mobile phone or from a personal computer. Likizo is a web based application that is accessible by phone, desktop, PC and tablets.</p>
+                        <a  href="#about">Read More</a>
+                     </div>
+                  </div>
+               </div>
+               <div class="carousel-item">
+                  <img class="third-slide" src="images/banner4.jpg" alt="Third slide">
+                  <div class="container">
+                     <div class="carousel-caption relative">
+                        <h1></h1>
+                        <p> TAKE A BREAK WITH A CLICK OF A BUTTON</p>
+                        <a  href="#about">Read More</a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+            <i class='fa fa-angle-left'></i>
+            </a>
+            <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+            <i class='fa fa-angle-right'></i>
+            </a>
+         </div>
+      </section>
+      <!-- about  -->
+      <div id="about" class="about top_layer">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="titlepage">
+                     <h2>About US</h2>
+                     <span>Likizo is an organization that offers employees’ absence and leave accruals services to firms, companies and organization. Likizo has been established recently with the aim of helping various human resource departments in assigning leave to staff member, manage employee’s absence, email notification, automated leave approval and report generation in a firm or organization from any region or locality at ease of a mobile phone or from a personal computer. Likizo is a web based application that is accessible by phone, desktop, PC and tablets.</span>
+                  </div>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="img-box">
+                     <figure><img src="images/about.jpg" alt="img"/></figure>
+                     <a href="#">Back home</a>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- end abouts -->
+
+      <!--  footer --> 
+      <footr>
+         <div class="footer">
+            <div class="container">
+               <div class="row">
+                  <div class="col-lg-2 col-md-6 col-sm-12 width">
+                     <div class="address">
+                        <h3>Address</h3>
+                        <i><img src="assets/home/icon/3.png">Nairobi CBD</i>
+                     </div>
+                  </div>
+                  <div class="col-lg-2 col-md-6 col-sm-12 width">
+                     <div class="address">
+                        <h3>Menus</h3>
+                        <i><img src="assets/home/icon/2.png">Likizo HQ</i>
+                     </div>
+                  </div>
+                  <div class="col-lg-2 col-md-6 col-sm-12 width">
+                     <div class="address">
+                        <h3>Useful Linkes</h3>
+                        <i><img src="assets/home/icon/1.png">www.Likizo.co.ke</i>
+                     </div>
+                  </div>
+                  <div class="col-lg-2 col-md-6 col-sm-12 width">
+                     <div class="address">
+                        <h3>Social Media </h3>
+                        <ul class="contant_icon">
+                           <li><img src="assets/home/icon/fb.png" alt="icon"/></li>
+                           <li><img src="assets/home/icon/tw.png" alt="icon"/></li>
+                           <li><img src="assets/home/icon/lin (2).png" alt="icon"/></li>
+                           <li><img src="assets/home/icon/instagram.png" alt="icon"/></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-xl-3 col-lg-3 col-md-12 col-sm-12 width">
+                     <div class="address">
+                        <h3>Newsletter </h3>
+                        <input class="form-control" placeholder="Enter your email" type="type" name="Enter your email">
+                        <button class="submit-btn">Subscribe</button>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="copyright">
+               <p>Copyright &copy; LIKIZO. All rights reserved.&copy;<script>document.write(new Date().getFullYear());</script></p>
+            </div>
+         </div>
+      </footr>
+      <!-- end footer -->
+      <!-- Javascript files--> 
+      <script src="assets/home/js/jquery.min.js"></script>
+      <script src="assets/home/js/popper.min.js"></script>
+      <script src="assets/home/js/bootstrap.bundle.min.js"></script>
+      <script src="assets/home/js/jquery-3.0.0.min.js"></script>
+      <script src="assets/home/js/plugin.js"></script>
+      <!-- sidebar --> 
+      <script src="assets/home/js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="assets/home/js/custom.js"></script>
+      <script>
+         $(document).ready(function(){
+         $(".fancybox").fancybox({
+         openEffect: "none",
+         closeEffect: "none"
+         });
+         
+         $(".zoom").hover(function(){
+         
+         $(this).addClass('transition');
+         }, function(){
+         
+         $(this).removeClass('transition');
+         });
+         });
+         
+      </script> 
+   </body>
+</html>
